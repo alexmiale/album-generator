708870df98677d9e70d646bcee36b127547c30f4
Alex Miale <alexmiale2014@gmail.com>
2024-05-03 15:55:51 -0400

    buffer issue

--- src/App.tsx
+++ src/App.tsx
@@ -1,23 +1,20 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import authorize from './authorize';
 
 function App() {
+  let data;
+  try {
+    data = authorize;
+  } catch (error) {
+    data = error;
+  }
+
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <data></data>
       </header>
     </div>
   );
