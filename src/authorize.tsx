ab2748217a61c1f1b1bed8597f37654fb79bee9f
Alex Miale <alexmiale2014@gmail.com>
2024-05-03 15:20:35 -0400

    get authorization

---
+++ src/authorize.tsx
@@ -0,0 +1,21 @@
+let client_id = '';
+let client_secret = '';
+
+async function getToken() {
+  const response = await fetch('https://accounts.spotify.com/api/token', {
+    method: 'POST',
+    body: new URLSearchParams({
+      'grant_type': 'client_credentials',
+    }),
+    headers: {
+      'Content-Type': 'application/x-www-form-urlencoded',
+      'Authorization': 'Basic ' + (Buffer.from(client_id + ':' + client_secret).toString('base64')),
+    },
+  });
+
+  return await response.json();
+}
+
+getToken().then((response) => {
+  console.log(response)
+});
